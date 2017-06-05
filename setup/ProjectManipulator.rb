4ef077fee6f86c92d9a67b2ae8e709089475da6c
hjayth <18622995206@163.com>
2017-06-06 00:58:36 +0800

    no message

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -91,12 +91,11 @@ RUBY
       # shared schemes have project specific names
     
       puts project_folder + "lla"
-      puts prefix
       puts @configurator.user_name
       puts @configurator.date
       puts @configurator.year
       puts @configurator.pod_name
-      puts @prefix
+    
       
       scheme_path = project_folder + "/PROJECT.xcodeproj/xcshareddata/xcschemes/"
       File.rename(scheme_path + "PROJECT.xcscheme", scheme_path +  @configurator.pod_name + "-Example.xcscheme")
