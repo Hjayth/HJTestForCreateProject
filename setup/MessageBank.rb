09d69d2b37af6f304e44c3f7487744f2c3936334
hjayth <18622995206@163.com>
2017-06-06 13:53:00 +0800

    no message

--- setup/MessageBank.rb
+++ setup/MessageBank.rb
@@ -36,19 +36,19 @@ module Pod
 
       puts "\n------------------------------"
       puts ""
-      puts "To get you started we need to ask a few questions, this should only take a minute."
+      puts "To get you started we need to set a Fileprefix"
       puts ""
 
       has_run_before = `defaults read org.cocoapods.pod-template HasRunbefore`.chomp == "1"
 
-      puts "If this is your first time we recommend running through with the guide: "
-      puts " - "  + "http://guides.cocoapods.org/making/using-pod-lib-create.html".blue.underlined
+# puts "If this is your first time we recommend running through with the guide: "
+#    puts " - "  + "http://guides.cocoapods.org/making/using-pod-lib-create.html".blue.underlined
 
-      if ENV["TERM_PROGRAM"] == "iTerm.app"
-        puts " ( hold cmd and click links to open in a browser. )".magenta
-      else
-        puts " ( hold cmd and double click links to open in a browser. )".magenta
-      end
+#   if ENV["TERM_PROGRAM"] == "iTerm.app"
+#     puts " ( hold cmd and click links to open in a browser. )".magenta
+#     else
+#      puts " ( hold cmd and double click links to open in a browser. )".magenta
+#     end
 
       unless has_run_before
         puts "\n Press return to continue."
@@ -61,7 +61,7 @@ module Pod
     def farewell_message
       puts ""
 
-      puts " Ace! you're ready to go!"
+      puts " come on ! you're ready to go!"
       puts " We will start you off by opening your project in Xcode"
       pod_name = @configurator.pod_name
       run_command "open 'Example/#{pod_name}.xcworkspace'", "open '#{pod_name}/Example/#{pod_name}.xcworkspace'"
