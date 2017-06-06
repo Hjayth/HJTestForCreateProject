7d6cf37319ccefe47718a861e70d147f326b909f
hjayth <18622995206@163.com>
2017-06-06 14:40:41 +0800

    no message

--- createProject.sh
+++ createProject.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#if [ -z "$1" ];then
-# echo "please input parameter"
-#exit
-#else
-    echo $Repository_name
+if [ -z "$1" ];then
+echo "you should use this shell like this - ""create.sh [your project name]" " please input parameter"
+exit
+else
+
+    echo $1
     #clone 远程基项目
     git clone  git@github.com:Hjayth/HJTestForCreateProject.git $1
         if [ $?  -eq 0 ]
         then
             cd $1
 #运行配置脚本
-
-            echo "create and configure your project...."
+            echo " this is the test for $1:" $1
             ./configure $1
-            echo "compelete create and configure your project"
+            echo "configure your project...."
         else
             echo "your remote repository is not available"
             exit 1
         fi
 
-#fi
+fi
 
 exit 0
