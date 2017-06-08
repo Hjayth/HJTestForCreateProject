56777ce19c1a0e480dd025dbf73ee5c3519552da
hjayth <18622995206@163.com>
2017-06-08 15:56:21 +0800

    no message

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -46,20 +46,20 @@ module Pod
     end
 
     def remove_demo_project
-      app_project = @project.native_targets.find { |target| target.product_type == "com.apple.product-type.application" }
-      test_target = @project.native_targets.find { |target| target.product_type == "com.apple.product-type.bundle.unit-test" }
-      test_target.name = @configurator.pod_name + "_Tests"
+        # app_project = @project.native_targets.find { |target| target.product_type == "com.apple.product-type.application" }
+        #test_target = @project.native_targets.find { |target| target.product_type == "com.apple.product-type.bundle.unit-test" }
+        #test_target.name = @configurator.pod_name + "_Tests"
      
       
       # Remove the implicit dependency on the app
-      test_dependency = test_target.dependencies.first
-      test_dependency.remove_from_project
-      app_project.remove_from_project
+      # test_dependency = test_target.dependencies.first
+      #  test_dependency.remove_from_project
+      # app_project.remove_from_project
 
       # Remove the build target on the unit tests
-      test_target.build_configuration_list.build_configurations.each do |build_config|
-        build_config.build_settings.delete "BUNDLE_LOADER"
-      end
+      #test_target.build_configuration_list.build_configurations.each do |build_config|
+      #  build_config.build_settings.delete "BUNDLE_LOADER"
+      # end
 
       # Remove the references in xcode
       # project_app_group = @project.root_object.main_group.children.select { |group| group.display_name.end_with? @configurator.pod_name }.first
