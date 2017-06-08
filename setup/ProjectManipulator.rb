6f115dd8602c6d1e55112ec82ad6c20d294a5b2e
hjayth <18622995206@163.com>
2017-06-08 16:20:19 +0800

    no message

--- setup/ProjectManipulator.rb
+++ setup/ProjectManipulator.rb
@@ -66,7 +66,7 @@ post_install do |installer|
             puts target.name
             if config.name == 'Debug'
                 puts config.build_settings['ONLY_ACTIVE_ARCH']
-                config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)', 'DEBUG=2']
+                #config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)', 'DEBUG=2']
                 config.build_settings['ONLY_ACTIVE_ARCH'] = 'NO'
                 puts config.build_settings['ONLY_ACTIVE_ARCH']
             end
